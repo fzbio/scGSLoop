b27f1dd000e27daf541e158649ecafa7e89b3bc0
Fuzhou Wang <wangfuzhou1997@gmail.com>
2023-08-04 15:55:24 +0800

    Update configs.py to change demo dataset

--- configs.py
+++ configs.py
@@ -1,24 +1,24 @@
 import torch
 
-SCOOL_100KB = 'data/human_prefrontal_cortex/luo_100kb_filtered.scool'
-SCOOL_10KB = 'data/human_prefrontal_cortex/luo_10kb_filtered.scool'
+SCOOL_100KB = 'data/mES/nagano_100kb_filtered.scool'
+SCOOL_10KB = 'data/mES/nagano_10kb_filtered.scool'
 
-MODEL_ID = 'mES_k3_GNNFINE'
-CHROMOSOMES = ['chr' + str(i) for i in range(1, 23)]
+MODEL_ID = 'hpc_k3_GNNFINE'
+CHROMOSOMES = ['chr' + str(i) for i in range(1, 20)]
 
 MODEL_DIR = 'models'
 OUT_DIR = f'preds/demo_output'
 THRESHOLD = 0.5
 
-MOTIF_FEATURE_PATH = f'data/graph_features/human/CTCF_hg19.10kb.input.csv'
-KMER_FEATURE_PATH = f'data/graph_features/human/hg19.10kb.kmer.csv'
+MOTIF_FEATURE_PATH = f'data/graph_features/mouse/CTCF_mm10.10kb.input.csv'
+KMER_FEATURE_PATH = f'data/graph_features/mouse/mm10.10kb.kmer.csv'
 
 IMPUTE = True
 OUT_IMPUTED_SCOOL_100KB = f'refined_scools/demo_imputed_coarse.scool'
 OUT_IMPUTED_SCOOL_10KB = f'refined_scools/demo_imputed_finer.scool'
-IMPUTATION_DATASET_DIR = 'data/human_prefrontal_cortex/demo_imputation_dataset_no_label'
+IMPUTATION_DATASET_DIR = 'data/mES/demo_imputation_dataset_no_label'
 
-GENOME_REGION_FILTER = 'region_filter/hg19_filter_regions.txt'  # Can be None
+GENOME_REGION_FILTER = 'region_filter/mm10_filter_regions.txt'  # Can be None
 
 
 
